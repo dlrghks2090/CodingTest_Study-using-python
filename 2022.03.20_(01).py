606a087f284c4b68287b0a19e7d4c1a7fad0f222
dlrghks2090 <dlrghks2090@gmail.com>
2022-03-20 21:02:44 +0900

    [baekjoon_큐_실버4_10845번_큐 문제]

---
+++ 2022.03.20_(01).py
@@ -0,0 +1,34 @@
+# 백준_큐_실버4_10845번_큐 문제
+import sys
+
+N = int(sys.stdin.readline())
+
+queue = []
+
+for i in range(N):
+    command = sys.stdin.readline().split()
+
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if not queue:
+            print(-1)
+        else:
+            print(queue.pop(0))
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if not queue:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[0])
+    elif command[0] == 'back':
+        if not queue:
+            print(-1)
+        else:
+            print(queue[-1])
