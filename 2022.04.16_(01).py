dc84025a39d5bfea7e7b86e45e6ee00b1f582d77
dlrghks2090 <dlrghks2090@gmail.com>
2022-04-16 17:14:34 +0900

    [baekjoon_브루트포스_1759번_골드5_암호 만들기 문제]

---
+++ 2022.04.16_(01).py
@@ -0,0 +1,34 @@
+# 백준_브루트포스_1759번_골드5_암호 만들기 문제
+L, C = map(int, input().split())
+
+arr = []
+arr = input().split()
+
+mo = "aeiou"
+mo = list(mo)
+
+sol = [0 for _ in range(L)]
+arr.sort()
+
+
+def back(a, b):
+    count = 0
+    if a == L:
+        for i in range(L):
+            for k in mo:
+                if sol[i] == k:
+                    count += 1
+        if count > 0 and L-count > 1:
+            for idx in sol:
+                print(idx, end='')
+            print()
+        return
+    elif b >= C:
+        return
+
+    for j in range(b, C):
+        sol[a] = arr[j]
+        back(a+1, j+1)
+
+
+back(0, 0)
